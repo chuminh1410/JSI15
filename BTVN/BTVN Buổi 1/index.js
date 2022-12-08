c7d8243973601a304e0d4ebdea214c3e5cdaaa13
chuminh1410 <chungocquangminh@gmail.com>
2022-12-08 11:26:58 +0700

    update 2

--- BTVN/BTVN Buổi 1/index.js
+++ BTVN/BTVN Buổi 1/index.js
@@ -3,15 +3,15 @@ let searchBtn = document.getElementById('button1').addEventListener('click', myF
 
 async function myFunction() {
     const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '6d0a49ad1amsha8da871af40ca9fp1e3017jsn1f71a1fca7a4',
-		'X-RapidAPI-Host': 'covid-19-coronavirus-statistics.p.rapidapi.com'
-	}
-};
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '6d0a49ad1amsha8da871af40ca9fp1e3017jsn1f71a1fca7a4',
+            'X-RapidAPI-Host': 'covid-19-coronavirus-statistics.p.rapidapi.com'
+        }
+    };
 
-await fetch(`https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=${searchText}`, options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+    await fetch(`https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total?country=${searchText}`, options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
 }
