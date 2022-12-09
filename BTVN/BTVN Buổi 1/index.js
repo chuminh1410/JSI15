e66e197f440e93298db2b0a9c62649fdf2b9c679
chuminh1410 <chungocquangminh@gmail.com>
2022-12-09 14:04:30 +0700

    done js (waiting to push into html)

--- BTVN/BTVN Buổi 1/index.js
+++ BTVN/BTVN Buổi 1/index.js
@@ -14,6 +14,27 @@ async function myFunction() {
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
-        document.getElementById("country").value = "";
+    document.getElementById("country").value = "";
 }
 
+function displayCountry(data) {
+    const comfirmed = data.confirmed;
+    const deaths = data.deaths;
+    const last_checked = data.lastChecked;
+    const last_report = data.lastReported;
+    const location = data.location;
+    const recovered = data.recovered;
+    const div1 = document.getElementById("return_1");
+
+    const return_comfirmed = comfirmed.strconfirmed;
+    const return_deaths = comfirmed.strdeaths;
+    const return_lastchecked = comfirmed.strlast_checked;
+    const return_lastreport = comfirmed.strlast_report;
+    const return_location = comfirmed.strlocation;
+    const return_recovered = comfirmed.strrecovered;
+
+    const heading = document.createElement("h1");
+    heading.innerHTML = information;
+    div1.appendChild(heading);
+
+}   
