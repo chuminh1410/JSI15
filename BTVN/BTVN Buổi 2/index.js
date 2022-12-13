d732ef11939926aeb74014f45acd3275d718a3d9
chuminh1410 <chungocquangminh@gmail.com>
2022-12-13 08:26:47 +0700

    BTVN 2 lan 1

---
+++ BTVN/BTVN Buổi 2/index.js
@@ -0,0 +1,16 @@
+// Task 1
+arr1 = [1,2,4,5,6]; 
+arr2 = [1,6,8,9,0];
+const fil = arr1.filter(value => arr2.includes(value));
+console.log(fil);
+
+// Task 2
+arr = [1,54,6,7];
+const newArr = arr.map(arr => {
+    return arr + 5;
+});
+console.log(newArr);
+
+// Task 3
+m = [1,2,4,5,6,7]; 
+n = [3,5,675,8,96];
