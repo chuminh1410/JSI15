7f90dded4ba9fd17bcf35eb80b32a9bc096c0be1
chuminh1410 <chungocquangminh@gmail.com>
2022-12-14 11:45:07 +0700

    task 3 going on

--- BTVN/BTVN Buổi 2/index.js
+++ BTVN/BTVN Buổi 2/index.js
@@ -1,6 +1,8 @@
 // Task 1
 arr1 = [1,2,4,5,6]; 
 arr2 = [1,6,8,9,0];
+console.log(arr1);
+console.log(arr2);
 const fil = arr1.filter(value => arr2.includes(value));
 console.log(fil);
 
@@ -14,3 +16,6 @@ console.log(newArr);
 // Task 3
 m = [1,2,4,5,6,7]; 
 n = [3,5,675,8,96];
+
+grouped_array = n.slice(0,5)
+console.log(grouped_array)
