c5e1b3c1296a174da8c358211e9e0f0c8b1009ff
chuminh1410 <chungocquangminh@gmail.com>
2022-12-17 19:07:16 +0700

    count shitt

---
+++ COUNT/index.js
@@ -0,0 +1,55 @@
+var clicks = 0;
+
+function onClick() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
+};
+
+var clicks1 = 0;
+
+function on1Click() {
+  clicks1 += 1;
+  document.getElementById("1clicks").innerHTML = clicks1;
+};
+
+var clicks2 = 0;
+
+function on2Click() {
+  clicks2 += 1;
+  document.getElementById("2clicks").innerHTML = clicks2;
+};
+
+var clicks3 = 0;
+
+function on3Click() {
+  clicks3 += 1;
+  document.getElementById("3clicks").innerHTML = clicks3;
+};
+
+var clicks4 = 0;
+
+function on4Click() {
+  clicks4 += 1;
+  document.getElementById("4clicks").innerHTML = clicks4;
+};
+
+var clicks5 = 0;
+
+function on5Click() {
+  clicks5 += 1;
+  document.getElementById("5clicks").innerHTML = clicks5;
+};
+
+var clicks6 = 0;
+
+function on6Click() {
+  clicks6 += 1;
+  document.getElementById("6clicks").innerHTML = clicks6;
+};
+
+var clicks7 = 0;
+
+function on7Click() {
+  clicks7 += 1;
+  document.getElementById("7clicks").innerHTML = clicks7;
+};
